40bc4000a3d9a4b43c928cb2d8c502f2f51cac3e
Kristóf Neller <neller.kristof@outlook.com>
2024-11-07 13:34:44 +0100

    .

--- 11.06/vite.config.js
+++ 11.06/vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/mapgame/',
+  base: '/PortfolioNK/11.06/docs/',
     build: {
         outDir: 'docs',
         emptyOutDir: true
